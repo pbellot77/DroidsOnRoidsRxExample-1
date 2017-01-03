183c24140815c7ca1d151d637ba2007aff43971a
Patrick <patrickbellot77@gmail.com>
2017-01-02 19:49:16 -0600

    Initial Commit

---
+++ DroidsOnRoidsRxExample#1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DroidsOnRoidsRxExample#1
+//
+//  Created by Patrick Bellot on 1/2/17.
+//  Copyright © 2017 Bell OS, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
